206d40f97496ff3626dea34249d44c5739107dea
monitoreolomas <campuzanonicolas012@gmail.com>
2026-05-21 17:15:12 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 // ── CONFIG — reemplazar con tus valores de Supabase ──────────
-const SUPABASE_URL = "https://ygwjvkjrpojxjczcholu.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://ygwjvkjrpojxjczcholu.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlnd2p2a2pycG9qeGpjemNob2x1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkzNzgyNDYsImV4cCI6MjA5NDk1NDI0Nn0.NvCxB2sXVxa4kQVGiVPs6_x1cinRi4UFpBJud6sx1Nw";
 // ────────────────────────────────────────────────────────────
 
